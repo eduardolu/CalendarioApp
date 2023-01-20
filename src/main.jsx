4b26fc57de8102df3a8c89c6d51c88cadc0f1154
eduardolu <edu291086@gmail.com>
2023-01-20 21:26:27 +0100

    inicio de proyecto

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {CalendarAPP} from './CalendarAPP'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CalendarAPP />
+  </React.StrictMode>,
+)
